dcbc9a06b8646bce61e3e672e1cf88d78d1b1e80
Jibo He <drhejibo@gmail.com>
2017-09-15 13:23:19 -0500

    update ch11-add figure, and Visual.Circle module

---
+++ Chapter 11 - PsychoPy入门 - 基本刺激材料的呈现/ch11-VisualCircleStim.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+from psychopy import visual, core, event
+
+#create a window to draw in
+myWin = visual.Window((800.0,800.0),allowGUI=False,winType='pyglet',
+            monitor='testMonitor', units ='deg', screen=0)
+myWin.setRecordFrameIntervals()
+
+
+#INITIALISE SOME STIMULI
+circle = visual.Circle(myWin, radius=2, edges=32)
+circle.pos=(0,0)
+circle.draw()
+
+myWin.flip()
+#pause, so you get a chance to see it!
+core.wait(5.0)
+
+
--- Chapter 11 - PsychoPy入门 - 基本刺激材料的呈现/shape-square.py
+++ Chapter 11 - PsychoPy入门 - 基本刺激材料的呈现/shape-square.py
@@ -1,21 +1,23 @@
+'''
+By DR. Jibo He @ uSEE Eye Tracking Inc. 
+Written for my student at Chinese Academy of Science.
+September 12, 2017
+'''
 #!/usr/bin/env python2
 from psychopy import visual, core, event
 
 #create a window to draw in
-myWin = visual.Window((800.0,800.0),allowGUI=False,winType='pyglet',
+myWin = visual.Window((200.0,200.0),allowGUI=False,winType='pyglet',
             monitor='testMonitor', units ='deg', screen=0)
 myWin.setRecordFrameIntervals()
-#choose some fonts. If a list is provided, the first font found will be used.
-fancy = ['Monotype Corsiva', 'Palace Script MT', 'Edwardian Script ITC']
 
 #INITIALISE SOME STIMULI
 square = visual.Rect(myWin,width=1,height=1)
 square.pos= (0,0)
+square.ori = 45
 square.draw()
     
 
 myWin.flip()
 #pause, so you get a chance to see it!
 core.wait(5.0)
-
-

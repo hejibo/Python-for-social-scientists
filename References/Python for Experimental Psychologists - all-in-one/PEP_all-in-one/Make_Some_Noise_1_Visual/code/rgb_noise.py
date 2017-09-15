24026f0bcd905cb27a903d4cbcafb43643176221
Jibo He <drhejibo@gmail.com>
2017-09-15 12:36:15 -0500

    add chapter 11 to 14

--- References/Python for Experimental Psychologists - all-in-one/PEP_all-in-one/Make_Some_Noise_1_Visual/code/rgb_noise.py
+++
@@ -1,7 +0,0 @@
-import numpy
-from matplotlib import pyplot
-
-noise = numpy.random.rand(500, 500, 3)
-pyplot.imshow(noise)
-
-pyplot.show()
--- References/Python for Experimental Psychologists - all-in-one/PEP_all-in-one/Make_Some_Noise_3_Musical_Instrument/code/constants.py
+++
@@ -1,4 +0,0 @@
-# set the display type to 'pygame'
-DISPTYPE = 'pygame'
-# make sure that the DISPSIZE matches your monitor resolution!
-DISPSIZE = (1920, 1080)
